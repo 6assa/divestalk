f4a88dd4a3a5f835c80af720ebc0b8ab743aa3da
user <user@6DF7103>
2021-11-15 23:11:41 +0900

    loginServlet(ユーザチェックなし)作成

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,6 +1,7 @@
 
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,16 +19,38 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("login.jsp").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		//リクエスト元からフォーム情報取得
+		String email=request.getParameter("mailAddress");//メールアドレス
+		String password=request.getParameter("password");//パスワード
+		
+		//エラーメッセージ
+		StringBuilder errormsg=new StringBuilder();
+		if(email==null||email.length()==0) {
+			//メールアドレス未入力
+			errormsg.append("メールアドレスが入力されていません。");
+		}
+		if(password==null||password.length()==0) {
+			//パスワード未入力
+			errormsg.append("<br>パスワードが入力されていません。");
+		}
+		
+		if(errormsg.length()>0) {
+			//エラーメッセージがある場合
+			request.setAttribute("errorMsg", errormsg.toString());
+			request.getRequestDispatcher("login.jsp").forward(request, response);
+		}
+		
+		//TODO ユーザチェック機能
+		
+		//成功時
+		response.sendRedirect("main.jsp");
 	}
 
 }
