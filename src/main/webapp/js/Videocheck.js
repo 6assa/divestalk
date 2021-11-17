169e0ac320ae6bd6412234f5153aa1a61783863c
mid2r <mid2r@LAPTOP-ED249K21>
2021-11-17 11:23:36 +0900

    callvideo.jspを、skywayを元に手を加えました。videocheck.htmlとvideocheck.jsは、skywayが提供しているサンプルコードをコピペしたものです。修正の参考にしたいです。

--- src/main/webapp/js/Videocheck.js
+++ src/main/webapp/js/Videocheck.js
@@ -1,71 +1,83 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://cdn.webrtc.ecl.ntt.com/skyway-4.4.3.js"></script>
-<title>DivesTalk ビデオ画面</title>
-</head>
-<body>
-<video id="my-video" width="400px" autoplay muted playsinline></video>
-<p id="my-id"></p>
-  <input id="their-id"></input>
-  <button id="make-call">発信</button>
-  <video id="their-video" width="400px" autoplay muted playsinline></video>
-<script>
-  let localStream;
+const Peer = window.Peer;
 
-  // カメラ映像取得
-  navigator.mediaDevices.getUserMedia({video: true, audio: true})
-    .then( stream => {
-    // 成功時にvideo要素にカメラ映像をセットし、再生
-    const videoElm = document.getElementById('my-video');
-    videoElm.srcObject = stream;
-    videoElm.play();
-    // 着信時に相手にカメラ映像を返せるように、グローバル変数に保存しておく
-    localStream = stream;
-  }).catch( error => {
-    // 失敗時にはエラーログを出力
-    console.error('mediaDevice.getUserMedia() error:', error);
-    return;
-  });
-  //Peer作成
-  const peer = new Peer({
+(async function main() {
+  const localVideo = document.getElementById('js-local-stream');
+  const localId = document.getElementById('js-local-id');
+  const callTrigger = document.getElementById('js-call-trigger');
+  const closeTrigger = document.getElementById('js-close-trigger');
+  const remoteVideo = document.getElementById('js-remote-stream');
+  const remoteId = document.getElementById('js-remote-id');
+  const meta = document.getElementById('js-meta');
+  const sdkSrc = document.querySelector('script[src*=skyway]');
+
+  meta.innerText = `
+    UA: ${navigator.userAgent}
+    SDK: ${sdkSrc ? sdkSrc.src : 'unknown'}
+  `.trim();
+
+  const localStream = await navigator.mediaDevices
+    .getUserMedia({
+      audio: true,
+      video: true,
+    })
+    .catch(console.error);
+
+  // Render local stream
+  localVideo.muted = true;
+  localVideo.srcObject = localStream;
+  localVideo.playsInline = true;
+  await localVideo.play().catch(console.error);
+
+  const peer = (window.peer = new Peer({
     key: '791965d2-d252-4755-ae36-d34e02cf3518',
-    debug: 3
-  });
-  peer.on('open', () => {
-	    document.getElementById('my-id').textContent = peer.id;
-	});
-//発信処理
-  document.getElementById('make-call').onclick = () => {
-    const theirID = document.getElementById('their-id').value;
-    const mediaConnection = peer.call(theirID, localStream);
-    setEventListener(mediaConnection);
-  };
+    debug: 3,
+  }));
 
-  // イベントリスナを設置する関数
-  const setEventListener = mediaConnection => {
-    mediaConnection.on('stream', stream => {
-      // video要素にカメラ映像をセットして再生
-      const videoElm = document.getElementById('their-video')
-      videoElm.srcObject = stream;
-      videoElm.play();
+  // Register caller handler
+  callTrigger.addEventListener('click', () => {
+    // Note that you need to ensure the peer has connected to signaling server
+    // before using methods of peer instance.
+    if (!peer.open) {
+      return;
+    }
+
+    const mediaConnection = peer.call(remoteId.value, localStream);
+
+    mediaConnection.on('stream', async stream => {
+      // Render remote stream for caller
+      remoteVideo.srcObject = stream;
+      remoteVideo.playsInline = true;
+      await remoteVideo.play().catch(console.error);
     });
-  }
-//着信処理
+
+    mediaConnection.once('close', () => {
+      remoteVideo.srcObject.getTracks().forEach(track => track.stop());
+      remoteVideo.srcObject = null;
+    });
+
+    closeTrigger.addEventListener('click', () => mediaConnection.close(true));
+  });
+
+  peer.once('open', id => (localId.textContent = id));
+
+  // Register callee handler
   peer.on('call', mediaConnection => {
     mediaConnection.answer(localStream);
-    setEventListener(mediaConnection);
+
+    mediaConnection.on('stream', async stream => {
+      // Render remote stream for callee
+      remoteVideo.srcObject = stream;
+      remoteVideo.playsInline = true;
+      await remoteVideo.play().catch(console.error);
+    });
+
+    mediaConnection.once('close', () => {
+      remoteVideo.srcObject.getTracks().forEach(track => track.stop());
+      remoteVideo.srcObject = null;
+    });
+
+    closeTrigger.addEventListener('click', () => mediaConnection.close(true));
   });
-  peer.on('error', err => {
-	    alert(err.message);
-	});
-  peer.on('close', () => {
-	  alert('通信が切断しました。');
-	});
-</script>
-</body>
-</html>
+
+  peer.on('error', console.error);
+})();
