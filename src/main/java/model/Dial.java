f54a5037b938c77714374cd0da89a8544353ac55
mid2r <mid2r@LAPTOP-ED249K21>
2021-11-19 09:56:00 +0900

    名前変えた

---
+++ src/main/java/model/Dial.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Dial {
+	Uri uri = Uri.parse(“tel:1111111111”); //ここに、call.jspで入力された電話番号を代入するようにしたい
+	Intent i = new Intent(Intent.ACTION_CALL,uri);
+	startActivity(i);
+}
